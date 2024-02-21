66089e48abaa32f36f35e1c03e4bdf14fc081a68
Raj Verma <kumar.dharminder@gmail.com>
2024-02-21 22:56:40 +0530

    Create Header , Footer and Product-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './component/product/product-list/product-list.component';
+import { JuiceComponent } from './component/product/juice/juice.component';
+import { GroceriesComponent } from './component/product/groceries/groceries.component';
+import { AboutComponent } from './component/product/about/about.component';
+import { ContactComponent } from './component/product/contact/contact.component';
+import { CartComponent } from './component/product/cart/cart.component';
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component:ProductListComponent  },
+    { path: 'juice', component:JuiceComponent  },
+    { path: 'groceries', component: GroceriesComponent  },
+    { path: 'about', component:AboutComponent  },
+    { path: 'contact', component:ContactComponent  },
+    { path: 'cart', component:CartComponent  },
 
-export const routes: Routes = [];
+  
+];
---
+++ src/app/component/product/juice/juice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-juice',
+  standalone: true,
+  imports: [],
+  templateUrl: './juice.component.html',
+  styleUrl: './juice.component.scss'
+})
+export class JuiceComponent {
+
+}
